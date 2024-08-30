4c0777ce4884c4d55147e6041083e76c4d0d5533
vitelo-code <roher86@gmail.com>
2024-08-30 10:24:09 -0600

    Project creation: added auth-astro and tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
 import { defineConfig } from 'astro/config';
 
+import node from '@astrojs/node';
+import auth from 'auth-astro';
+
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [auth(), tailwind()],
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
+});
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,4 @@
 /// <reference path="../.astro/types.d.ts" />
+
+AUTH_SECRET = '8aac39b84470b990e775e9f519253c03';
+AUTH_TRUST_HOST = true;
